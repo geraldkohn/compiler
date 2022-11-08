eb2464181c1fb046b87e47db483bd17034fc5d2b
geraldkohn <dysonconrad@outlook.com>
2022-11-08 10:11:13 +0800

    更新配置

--- src/Config.java
+++ src/Config.java
@@ -1,22 +1,22 @@
 public class Config {
-    static String grammarPath = "C:\\MyWindows\\Document\\School\\编译原理\\compiler\\input\\grammer.txt";
-//    static String grammarPath = "C:\\MyWindows\\Document\\School\\编译原理\\compiler\\input\\grammer_test.txt";
+    static String grammarPath = ".\\input\\grammar.txt";
+//    static String grammarPath = ".\\input\\grammar_test.txt";
 
-//    static String lexInputPath = "C:\\MyWindows\\Document\\School\\编译原理\\compiler\\input\\1.txt";
-//    static String lexInputPath = "C:\\MyWindows\\Document\\School\\编译原理\\compiler\\input\\2.txt";
-//    static String lexInputPath = "C:\\MyWindows\\Document\\School\\编译原理\\compiler\\input\\3.txt";
-//    static String lexInputPath = "C:\\MyWindows\\Document\\School\\编译原理\\compiler\\input\\4.txt";
-    static String lexInputPath = "C:\\MyWindows\\Document\\School\\编译原理\\compiler\\input\\5.txt";
+//    static String lexInputPath = ".\\input\\1.txt";
+//    static String lexInputPath = ".\\input\\2.txt";
+//    static String lexInputPath = ".\\input\\3.txt";
+//    static String lexInputPath = ".\\input\\4.txt";
+    static String lexInputPath = ".\\input\\5.txt";
 
-    static String formulaPath = "C:\\MyWindows\\Document\\School\\编译原理\\compiler\\output\\文法.txt";
-    static String firstTablePath = "C:\\MyWindows\\Document\\School\\编译原理\\compiler\\output\\First集合.txt";
-    static String followTablePath = "C:\\MyWindows\\Document\\School\\编译原理\\compiler\\output\\Follow集合.txt";
-    static String predictMapPath = "C:\\MyWindows\\Document\\School\\编译原理\\compiler\\output\\分析表.txt";
-    static String terminalPath = "C:\\MyWindows\\Document\\School\\编译原理\\compiler\\output\\终结符.txt";
-    static String nonTerminalPath = "C:\\MyWindows\\Document\\School\\编译原理\\compiler\\output\\非终结符.txt";
-    static String lexiconMiddleResult = "C:\\MyWindows\\Document\\School\\编译原理\\compiler\\output\\词法分析产生的中间结果.txt";
-    static String parseResultPath = "C:\\MyWindows\\Document\\School\\编译原理\\compiler\\result\\语法分析结果.txt";
-    static String lexiconResultPath = "C:\\MyWindows\\Document\\School\\编译原理\\compiler\\result\\词法分析结果.txt";
+    static String formulaPath = ".\\output\\文法.txt";
+    static String firstTablePath = ".\\output\\First集合.txt";
+    static String followTablePath = ".\\output\\Follow集合.txt";
+    static String predictMapPath = ".\\output\\分析表.txt";
+    static String terminalPath = ".\\output\\终结符.txt";
+    static String nonTerminalPath = ".\\output\\非终结符.txt";
+    static String lexiconMiddleResult = ".\\output\\词法分析产生的中间结果.txt";
+    static String parseResultPath = ".\\result\\语法分析结果.txt";
+    static String lexiconResultPath = ".\\result\\词法分析结果.txt";
 
     static String initSymbol = "program"; // 入口文法！
 }
